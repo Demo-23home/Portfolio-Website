76022d965862b5fb34c0b22ff446e0aa82590939
Demo-23home <zeyadslama23@gmail.com>
2026-03-26 01:20:42 +0200

    fix: ui improvments

--- script.js
+++ script.js
@@ -1,6 +1,46 @@
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open")
-  icon.classList.toggle("open")
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
+
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("section-show");
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  },
+);
+
+sections.forEach((section) => {
+  section.classList.add("section-hidden");
+  observer.observe(section);
+});
+
+const links = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  document.querySelectorAll("section").forEach((section) => {
+    const sectionTop = section.offsetTop - 100;
+    if (scrollY >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  links.forEach((link) => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === `#${current}`) {
+      link.classList.add("active");
+    }
+  });
+});
